673d08ad345c0e748a9cb1efba113240acaa21d6
Karume-lab <danielkarume@outlook.com>
2023-11-11 07:11:07 +0300

    designed the frontend

---
+++ Todo/todo/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+const Footer = () => {
+	return (
+		<>
+			<footer className='footer'>
+				<span>Copyright &copy; 2023</span>
+				<span>Designed by <a href="https://github.com/Javan-Odhiambo">Javan</a></span>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
